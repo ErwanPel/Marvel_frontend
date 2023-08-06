db8104b4af27f914087faa93b052b1e1bb2dffff
Erwan <erwanpellerinpro@gmail.com>
2023-08-06 23:22:46 +0200

    fix JSON.parse trouble

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -15,53 +15,18 @@ export default function Cards({
   signModal,
   cookiesSort,
 }) {
-  // const handleFav = (id, favData, event) => {
-  //   event.preventDefault();
-  //   console.log(favData);
-  //   if (favData.title) {
-  //     console.log("comic", cookiesComics.indexOf(id));
-  //     const favori = favData.title;
-  //     console.log("comics");
-  //     if (!Cookies.get(id)) {
-  //       Cookies.set(id, id);
-  //       setCookiesComics((cookiesComics) => [...cookiesComics, id]);
-  //     } else {
-  //       Cookies.remove(id);
-  //       const cookiesArray = [...cookiesComics];
-  //       cookiesArray.splice(cookiesArray.indexOf(id), 1);
-  //       setCookiesComics(cookiesArray);
-  //     }
-  //   } else {
-  //     console.log("chara");
-  //     console.log("chara", cookiesChar.indexOf(id));
-  //     if (!Cookies.get(id)) {
-  //       const favori = favData.name;
-  //       Cookies.set(id, id);
-  //       setCookiesChar((cookiesChar) => [...cookiesChar, id]);
-  //     } else {
-  //       Cookies.remove(id);
-  //       const cookiesArray = [...cookiesChar];
-  //       cookiesArray.splice(cookiesArray.indexOf(id), 1);
-  //       setCookiesChar(cookiesArray);
-  //     }
-  //   }
-  // };
-
   console.log("sort", cookiesSort);
 
   const handleFav = (id, favData, event) => {
     event.preventDefault();
     if (favData.title) {
-      console.log("comics");
       if (Cookies.get("comics")) {
-        console.log("existe");
         let array = JSON.parse(Cookies.get("comics"));
-        console.log("prev", array);
+
         if (array.indexOf(id) === -1) {
           array.push(id);
           Cookies.set("comics", JSON.stringify(array));
           setCookiesComics((cookiesComics) => [...cookiesComics, id]);
-          console.log(JSON.parse(Cookies.get("comics")));
         } else {
           let array = JSON.parse(Cookies.get("comics"));
           array.splice(array.indexOf(id), 1);
@@ -69,25 +34,19 @@ export default function Cards({
           const cookiesArray = [...cookiesComics];
           cookiesArray.splice(cookiesArray.indexOf(id), 1);
           setCookiesComics(cookiesArray);
-          console.log("fin supp", JSON.parse(Cookies.get("comics")));
         }
       } else {
-        console.log("n'existe pas");
         Cookies.set("comics", JSON.stringify([id]));
         setCookiesComics((cookiesComics) => [...cookiesComics, id]);
-        console.log(JSON.parse(Cookies.get("comics")));
       }
     } else {
-      console.log("char");
       if (Cookies.get("characters")) {
-        console.log("existe");
         let array = JSON.parse(Cookies.get("characters"));
-        console.log("prev", array);
+
         if (array.indexOf(id) === -1) {
           array.push(id);
           Cookies.set("characters", JSON.stringify(array));
           setCookiesChar((cookiesChar) => [...cookiesChar, id]);
-          console.log(JSON.parse(Cookies.get("characters")));
         } else {
           let array = JSON.parse(Cookies.get("characters"));
           array.splice(array.indexOf(id), 1);
@@ -95,13 +54,10 @@ export default function Cards({
           const cookiesArray = [...cookiesChar];
           cookiesArray.splice(cookiesArray.indexOf(id), 1);
           setCookiesChar(cookiesArray);
-          console.log("fin supp", JSON.parse(Cookies.get("characters")));
         }
       } else {
-        console.log("n'existe pas");
         Cookies.set("characters", JSON.stringify([id]));
         setCookiesChar((cookiesChar) => [...cookiesChar, id]);
-        console.log(JSON.parse(Cookies.get("characters")));
       }
     }
   };
