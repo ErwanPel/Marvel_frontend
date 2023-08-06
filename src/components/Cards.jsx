120793f6d1a290f8f864c280d4693baf6e08ea90
Erwan <erwanpellerinpro@gmail.com>
2023-08-06 23:32:27 +0200

    without cookies

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Cookies from "js-cookie";
+import { uid } from "react-uid";
 
 import "../assets/css/cards.css";
 
@@ -17,50 +18,50 @@ export default function Cards({
 }) {
   console.log("sort", cookiesSort);
 
-  const handleFav = (id, favData, event) => {
-    event.preventDefault();
-    if (favData?.title) {
-      if (Cookies.get("comics")) {
-        let array = JSON.parse(Cookies.get("comics"));
+  // const handleFav = (id, favData, event) => {
+  //   event.preventDefault();
+  //   if (favData?.title) {
+  //     if (Cookies.get("comics")) {
+  //       let array = JSON.parse(Cookies.get("comics"));
 
-        if (array.indexOf(id) === -1) {
-          array.push(id);
-          Cookies.set("comics", JSON.stringify(array));
-          setCookiesComics((cookiesComics) => [...cookiesComics, id]);
-        } else {
-          let array = JSON.parse(Cookies.get("comics"));
-          array.splice(array.indexOf(id), 1);
-          Cookies.set("comics", JSON.stringify(array));
-          const cookiesArray = [...cookiesComics];
-          cookiesArray.splice(cookiesArray.indexOf(id), 1);
-          setCookiesComics(cookiesArray);
-        }
-      } else {
-        Cookies.set("comics", JSON.stringify([id]));
-        setCookiesComics((cookiesComics) => [...cookiesComics, id]);
-      }
-    } else {
-      if (Cookies.get("characters")) {
-        let array = JSON.parse(Cookies.get("characters"));
+  //       if (array.indexOf(id) === -1) {
+  //         array.push(id);
+  //         Cookies.set("comics", JSON.stringify(array));
+  //         setCookiesComics((cookiesComics) => [...cookiesComics, id]);
+  //       } else {
+  //         let array = JSON.parse(Cookies.get("comics"));
+  //         array.splice(array.indexOf(id), 1);
+  //         Cookies.set("comics", JSON.stringify(array));
+  //         const cookiesArray = [...cookiesComics];
+  //         cookiesArray.splice(cookiesArray.indexOf(id), 1);
+  //         setCookiesComics(cookiesArray);
+  //       }
+  //     } else {
+  //       Cookies.set("comics", JSON.stringify([id]));
+  //       setCookiesComics((cookiesComics) => [...cookiesComics, id]);
+  //     }
+  //   } else {
+  //     if (Cookies.get("characters")) {
+  //       let array = JSON.parse(Cookies.get("characters"));
 
-        if (array.indexOf(id) === -1) {
-          array.push(id);
-          Cookies.set("characters", JSON.stringify(array));
-          setCookiesChar((cookiesChar) => [...cookiesChar, id]);
-        } else {
-          let array = JSON.parse(Cookies.get("characters"));
-          array.splice(array.indexOf(id), 1);
-          Cookies.set("characters", JSON.stringify(array));
-          const cookiesArray = [...cookiesChar];
-          cookiesArray.splice(cookiesArray.indexOf(id), 1);
-          setCookiesChar(cookiesArray);
-        }
-      } else {
-        Cookies.set("characters", JSON.stringify([id]));
-        setCookiesChar((cookiesChar) => [...cookiesChar, id]);
-      }
-    }
-  };
+  //       if (array.indexOf(id) === -1) {
+  //         array.push(id);
+  //         Cookies.set("characters", JSON.stringify(array));
+  //         setCookiesChar((cookiesChar) => [...cookiesChar, id]);
+  //       } else {
+  //         let array = JSON.parse(Cookies.get("characters"));
+  //         array.splice(array.indexOf(id), 1);
+  //         Cookies.set("characters", JSON.stringify(array));
+  //         const cookiesArray = [...cookiesChar];
+  //         cookiesArray.splice(cookiesArray.indexOf(id), 1);
+  //         setCookiesChar(cookiesArray);
+  //       }
+  //     } else {
+  //       Cookies.set("characters", JSON.stringify([id]));
+  //       setCookiesChar((cookiesChar) => [...cookiesChar, id]);
+  //     }
+  //   }
+  // };
 
   return (
     <main className="wrapper cards-bloc">
@@ -70,7 +71,7 @@ export default function Cards({
         return (
           <>
             <Link
-              key={element._id}
+              key={uid(element)}
               to={`${path}${element._id}`}
               state={{ data: element }}
               className="card"
@@ -87,7 +88,7 @@ export default function Cards({
                     ? "favorite"
                     : "favorite  full-heart")
                 }
-                onClick={(event) => handleFav(element._id, element, event)}
+                // onClick={(event) => handleFav(element._id, element, event)}
               >
                 <FontAwesomeIcon className="heart" icon="fa-regular fa-heart" />
               </div>
